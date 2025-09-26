f1e3760c84c97706c39fb781721fe3c20ad38e06
devcarlosperez <carlosperezsantana04@gmail.com>
2025-09-26 18:49:24 +0100

    Integrated OMDB Api in movie controller

--- backend/controllers/movie.controller.js
+++ backend/controllers/movie.controller.js
@@ -1,74 +1,141 @@
 const db = require("../models")
+const axios = require("axios")
 const movieObject = db.movies
 const op = db.Sequelize.Op
 
-exports.create = (req, res) => {
-  const movie = {
-    name: req.body.name,
-    rating: req.body.rating
+exports.create = async (req, res) => {
+  const userTitle = req.body.name
+  const userRating = req.body.rating
+
+  // Petición a OMDB para obtener los datos de la película
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "ce2589c3",
+      t: userTitle,
+    },
+  });
+
+  // Almacenar la respuesta en una constante
+  const omdbData = response.data
+
+  if (omdbData.Response === "False") {
+    return res.status(404).send({ message: "Movie not found in OMDb" });
   }
 
-  movieObject.create(movie).then(data => {
-    res.send(data)
-  }).catch(err => {
-    res.status(500).send({
-      message: err.message || "Some error ocurred while creating the movie."
+  const movie = {
+    name: userTitle,
+    rating: userRating,
+    poster: omdbData.Poster,
+    year: omdbData.Year,
+    released: omdbData.Released,
+    runTime: omdbData.Runtime,
+    genre: omdbData.Genre,
+    director: omdbData.Director,
+    actors: omdbData.Actors,
+    language: omdbData.Language,
+    country: omdbData.Country,
+    plot: omdbData.Plot,
+  }
+
+  movieObject
+    .create(movie)
+    .then((data) => {
+      res.send(data);
     })
-  })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error ocurred while creating the movie.",
+      })
+    })
 }
 
 exports.findAll = (req, res) => {
-  movieObject.findAll().then(data => {
-    res.send(data)
-  }).catch(err => {
-    res.status(500).send({
-      message: err.message || "Some error ocurred while retrieving movies."
+  movieObject
+    .findAll()
+    .then((data) => {
+      res.send(data);
     })
-  })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error ocurred while retrieving movies.",
+      })
+    })
 }
 
 exports.findOne = (req, res) => {
   const movieId = req.params.id
 
-  movieObject.findOne({ where: {id: movieId} })
-    .then(data => {
-      res.send(data)
-    }).catch(err => {
+  movieObject
+    .findOne({ where: { id: movieId } })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
       res.status(500).send({
-        message: err.message || "Some error ocurred while retrieving the movie."
+        message:
+          err.message || "Some error ocurred while retrieving the movie.",
       })
     })
 }
 
-exports.update = (req, res) => {
+exports.update = async (req, res) => {
   const movieId = req.params.id
-  const movie = {
-    name: req.body.name,
-    rating: req.body.rating
+  const userTitle = req.body.name
+  const userRating = req.body.rating
+
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "ce2589c3",
+      t: userTitle,
+    }
+  })
+
+  const omdbData = response.data
+
+  if (omdbData.Response === "False") {
+    return res.status(404).send({ message: "Movie not found in OMDb" })
   }
 
-  movieObject.update(movie, {where: {id: movieId} })
-    .then(data => {
-      res.send(data)
-    }).catch(err => {
+  const movie = {
+    name: userTitle,
+    rating: userRating,
+    poster: omdbData.Poster,
+    year: omdbData.Year,
+    released: omdbData.Released,
+    runTime: omdbData.Runtime,
+    genre: omdbData.Genre,
+    director: omdbData.Director,
+    actors: omdbData.Actors,
+    language: omdbData.Language,
+    country: omdbData.Country,
+    plot: omdbData.Plot,
+  }
+
+  movieObject
+    .update(movie, { where: { id: movieId } })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
       res.status(500).send({
-        message: err.message || "Some error ocurred while updating the movie."
+        message: err.message || "Some error ocurred while updating the movie.",
       })
     })
 }
 
 exports.delete = (req, res) => {
-  const movieId = req.params.id
+  const movieId = req.params.id;
 
-  movieObject.destroy({ where: {id: movieId} })
-    .then(data => {
+  movieObject
+    .destroy({ where: { id: movieId } })
+    .then((data) => {
       res.send({
-        message: "Movie has been deleted."
-      });
+        message: "Movie has been deleted.",
+      })
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message: err.message || "Some error occurred while deleting the movie."
-      });
-    });
+        message: err.message || "Some error occurred while deleting the movie.",
+      })
+    })
 }
